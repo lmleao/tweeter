e159bf91007623de086e88fda4364570c2fa3459
Luiza Leao <luiza.leao96@gmail.com>
2023-12-16 18:35:39 -0500

    Initial commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+$(document).ready(function() {
+  const tweetText = $('#tweet-text');
+  const counter = tweetText.closest('.new-tweet').find('.counter');
+  counter.text(140);
+
+  tweetText.on('input', function() {
+    // Update the character count
+    const remainingChars = 140 - tweetText.val().length;
+    counter.text(remainingChars);
+
+
+    if (remainingChars < 0) {
+      counter.addClass('counter-exceeded');
+    } else {
+      counter.css('counter-exceeded');
+    }
+  });
+});
